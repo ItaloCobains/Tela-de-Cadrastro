5c90615e30fdf524bc950b01671bd1016806e86e
ItaloCobains <monokatchaleger@gmail.com>
2022-03-28 02:34:16 -0300

    forms

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ let B7validator = {
 
         let inputs = form.querySelectorAll('input');
 
+        B7validator.clearErrors();
+
         for(let i = 0; i<inputs.length; i++) {
             let input = inputs[i];
             let check = B7validator.checkInput(input);
             if (check !== true) {
                 send = false;
-                console.log(check);
+                B7validator.showError(input, check);
             }
         }
 
@@ -29,19 +31,42 @@ let B7validator = {
                 
                 switch(rDetails[0]){
                     case 'required':
-                        if(input.value == '') {
-                            return 'Campo nao pode ser vazio';
+                        if(input.value == ''){
+                            return 'Campo não pode ser vazio.'
                         }
                     break;
                     case 'min':
-                        
+
                     break;
                 }
             }
         }
 
         return true;
+    },
+    showError: (input, error) => {
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+
+    },
+    clearErrors: ()=>{
+        let inputs = form.querySelectorAll('input')
+
+        for(let i = 0; i < inputs.length; i++){
+            inputs[i].style = ''
+        }
+
+        let errorElements = document.querySelectorAll('.error')
+        for(let i=0;i<errorElements.length;i++){
+            errorElements[i].remove();
+        }
     }
+
 
 };
 
